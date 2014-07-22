cc783b993da2467724f3e2ed7827fd1258ae9bd5
Joshua Langholtz <jjlangholtz@gmail.com>
2014-07-22 10:06:17 -0400

    Add title and content specs for valid

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,3 @@
 class Question < ActiveRecord::Base
+  validates :title, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Question, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Question, type: :model do
+  it 'validates the presence of title' do
+    question = Question.new
+    question.save
+    expect(question).not_to be_valid
+    expect(question.errors).to have_key :title
+  end
+
+  it 'validates the presence of content' do
+    question = Question.new
+    question.save
+    expect(question).not_to be_valid
+    expect(question.errors).to have_key :content
+  end
 end
